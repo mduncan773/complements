8c860fd96f1eab7b7b1de53ed3e06bb0be679cb0
Miles Duncan <miles.c.duncan@gmail.com>
2016-02-20 12:57:03 -0600

    refactor API with underscore

--- complements_app/data.js
+++ complements_app/data.js
@@ -1,3 +1,5 @@
+
+
 var complements = [{
     id: 1,
     description: 'You look beautiful',
@@ -12,3 +14,13 @@ var complements = [{
     completed: true
 }];
 
+
+module.exports = complements;
+
+//module.exports = function(name, age) {
+//    this.name = name;
+//    this.age = age;
+//    this.about = function() {
+//        console.log(this.name +' is '+ this.age +' years old');
+//    };
+//};
--- complements_app/server.js
+++ complements_app/server.js
@@ -2,10 +2,13 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+//var data = require('./data.js')
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3330;
 var complementsNextId = 1; 
 var complements = [];
+
 
 app.use(bodyParser.json());
 
@@ -19,29 +22,41 @@ app.get('/complements', function(req, res){
     res.json(complements);
 });
 
+
 // GET /complements/:id
 app.get('/complements/:id', function(req, res){
     // itterate over todo's array find the match 
     var complementId = parseInt(req.params.id, 10);
-    var matchedComplement;
-    complements.forEach(function(complement){
-        if (complementId === complement.id){
-            matchedComplement = complement;
-            console.log(complement.description);
-        }
-    });
+    // get complement wehre id matches 
+    var matchedComplement = _.findWhere(complements, {id: complementId});
+//    var matchedComplement;
+//    complements.forEach(function(complement){
+//        if (complementId === complement.id){
+//            matchedComplement = complement;
+//            console.log(complement.description);
+//        }
+//    });
     
     if (matchedComplement) {
         res.json(matchedComplement);
     } else {
         res.status(404).send();
     }
-    //res.send('Asking for todo with id of ' + req.params.id);
-});
-
-//POST /complements
+    res.send('Asking for todo with id of ' + req.params.id);
+});//POST /complements
 app.post('/complements', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+   
+    // use pick 
+    
+    body.description = body.description.trim();
+    
+    
+    
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    } 
+    //.trim removes spaces before and after 
     // add id field 
     body.id = complementsNextId;
     complementsNextId ++;
@@ -51,7 +66,6 @@ app.post('/complements', function(req, res){
     console.log('ID: ' + body.id)
     res.json(body);
 });
-
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
